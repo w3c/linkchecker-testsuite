cec9012faa9ba6d2caa930feea125f24bcd0d99c
ot <ot>
2008-01-28 02:15:28 +0000

    trap resource for ill-calculated base URI

---
+++ trap/http.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>untitled</title>
+	<meta name="author" content="olivier Thereaux">
+</head>
+<body>
+
+</body>
+</html>
